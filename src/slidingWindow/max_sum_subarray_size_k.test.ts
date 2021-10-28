7e13659eab7cbc9d181bea05ab52af2faa2dc445
Jun Guo <gjuoun@gmail.com>
2021-10-27 23:35:59 -0300

    update yarn version

--- src/slidingWindow/max_sum_subarray_size_k.test.ts
+++ src/slidingWindow/max_sum_subarray_size_k.test.ts
@@ -17,14 +17,14 @@ const max_sub_array_of_size_k = function (k: number, arr: number[]): number {
 
 
 describe("max_sub_array_of_size_k", () => {
-  it("Input: [2, 1, 5, 1, 3, 2], Output: 9", () => {
+  it("Input: [2, 1, 5, 1, 3, 2], k = 3,Output: 9", () => {
     const k = 3
     const arr = [2, 1, 5, 1, 3, 2]
     const output = 9
     expect(max_sub_array_of_size_k(k, arr)).toBe(output)
   });
 
-  it("Input: [2, 3, 4, 1, 5], Output: 9", () => {
+  it("Input: [2, 3, 4, 1, 5], k = 2, Output: 7", () => {
     const k = 2
     const arr = [2, 3, 4, 1, 5]
     const output = 7

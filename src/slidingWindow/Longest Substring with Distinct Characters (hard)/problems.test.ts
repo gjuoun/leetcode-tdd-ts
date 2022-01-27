94d58c5d4ea27b8ef463e87c5129dd4d36a4abf5
Jun <gjuoun@gmail.com>
2022-01-26 23:43:09 -1000

    newer solution

--- src/slidingWindow/Longest Substring with Distinct Characters (hard)/problems.test.ts
+++ src/slidingWindow/Longest Substring with Distinct Characters (hard)/problems.test.ts
@@ -1,27 +1,51 @@
 
 
+// const length_of_longest_substring = (input: string) => {
+//   let startIndex = 0, maxLength = 0
+//   let map = new Map<string, number>()
+
+//   for (let endIndex = 0; endIndex < input.length; endIndex++) {
+//     const rightChar = input[endIndex]
+
+//     if (map.has(rightChar)) {
+//       startIndex =  Math.max(startIndex ,map.get(rightChar)! + 1)
+//     }
+
+
+//     map.set(rightChar, endIndex)
+
+//     const windowSize = (endIndex + 1) - startIndex
+//     maxLength = Math.max(windowSize, maxLength)
+//   }
+
+//   return maxLength
+
+// }
+
+
 const length_of_longest_substring = (input: string) => {
-  let startIndex = 0, maxLength = 0
-  let map = new Map<string, number>()
+  let windowStart = 0, maxLength = 0
+  let map: Record<string, number> = {}
 
-  for (let endIndex = 0; endIndex < input.length; endIndex++) {
-    const rightChar = input[endIndex]
-
-    if (map.has(rightChar)) {
-      startIndex =  Math.max(startIndex ,map.get(rightChar)! + 1)
+  for(let windowEnd = 0; windowEnd< input.length; windowEnd ++){
+    let rightChar = input[windowEnd]
+    
+    // a: 0 may exist
+    if(map[rightChar] >= 0){
+      windowStart = Math.max(windowStart, map[rightChar] + 1)
     }
 
+    map[rightChar] = windowEnd
 
-    map.set(rightChar, endIndex)
+    const windowSize = (windowEnd - windowStart) + 1
 
-    const windowSize = (endIndex + 1) - startIndex
-    maxLength = Math.max(windowSize, maxLength)
+    maxLength = Math.max(maxLength, windowSize)
+
   }
 
   return maxLength
 
 }
-
 
 
 

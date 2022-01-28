1ff1a498b894888f43fa9700456865bea3a84de3
Jun Guo <gjuoun@gmail.com>
2022-01-27 23:52:27 -0800

    update problem statement

--- src/slidingWindow/Longest Substring with Same Letters after Replacement (hard)/problems.test.ts
+++ src/slidingWindow/Longest Substring with Same Letters after Replacement (hard)/problems.test.ts
@@ -1,4 +1,28 @@
 
+/**
+Problem Statement#
+Given a string with lowercase letters only, if you are allowed to replace no more than k letters with any letter, find the length of the longest substring having the same letters after replacement.
+
+Example 1:
+
+Input: String="aabccbb", k=2
+Output: 5
+Explanation: Replace the two 'c' with 'b' to have the longest repeating substring "bbbbb".
+
+Example 2:
+
+Input: String="abbcb", k=1
+Output: 4
+Explanation: Replace the 'c' with 'b' to have the longest repeating substring "bbbb".
+
+Example 3:
+
+Input: String="abccde", k=1
+Output: 3
+Explanation: Replace the 'b' or 'd' with 'c' to have the longest repeating substring "ccc".
+
+ */
+
 const length_of_longest_substring_same = (input: string, k: number) => {
   let windowStart = 0, windowEnd = 0;
   let maxRepeatCount = 0, max = 0;

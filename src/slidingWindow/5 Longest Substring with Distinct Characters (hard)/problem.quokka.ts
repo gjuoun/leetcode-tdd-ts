6869dee6b2afd5c5ca67473c722a5b0a43cac738
Jun Guo <gjuoun@gmail.com>
2022-01-31 04:21:05 -0400

    do another time

--- src/slidingWindow/5 Longest Substring with Distinct Characters (hard)/problem.quokka.ts
+++ src/slidingWindow/5 Longest Substring with Distinct Characters (hard)/problem.quokka.ts
@@ -21,5 +21,24 @@
 // }
 
 
+export const longest_substring_with_distinct_characters_quokka = (input: string) => {
+  let windowStart = 0, maxLength = 0;
+  let map: Record<string , number> = {}
+
+  for(let windowEnd = 0; windowEnd < input.length; windowEnd += 1){
+    const rightChar = input[windowEnd] 
+
+    if(rightChar in map){
+      windowStart = Math.max(windowStart, map[rightChar] + 1)
+    }
+
+    map[rightChar] = windowEnd
+
+    maxLength = Math.max(maxLength, windowEnd - windowStart + 1)
+  }
+
+  return maxLength
+ }
+
 
 console.log(longest_substring_with_distinct_characters_quokka('aabccbb')); //should be 3

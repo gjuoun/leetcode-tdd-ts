3f537a92f9ca73f07543afa42ccee17c54137f6c
Jun Guo <gjuoun@gmail.com>
2022-02-02 22:47:18 -0800

    redo challenge 1

--- src/slidingWindow/challenges/Challenge 1/problem.quokka.ts
+++ src/slidingWindow/challenges/Challenge 1/problem.quokka.ts
@@ -46,45 +46,99 @@
 //   return false
 // }
 
+// export const permutation_in_a_string_qk = (input: string, pattern: string) => {
+//   let windowStart = 0, maxLength = 0;
+//   let freqMap: Record<string, number> = {}
+//   let matchedCount = 0;
+
+//   for(let index = 0; index < pattern.length; index += 1){
+//     const char = pattern[index] //?
+//     if(char in freqMap){
+//       freqMap[char] += 1
+//     }else{
+//       freqMap[char] = 1
+//     }
+//   }
+
+//   freqMap
+
+//   for(let windowEnd = 0; windowEnd < input.length; windowEnd += 1){
+//     const rightChar = input[windowEnd]
+
+//     if(rightChar in freqMap){
+
+//       freqMap[rightChar] -= 1
+//       if(freqMap[rightChar] === 0){
+//         matchedCount += 1
+//       }
+
+//       if(matchedCount === Object.keys(freqMap).length){
+//         return true // matched all letters
+//       }
+//     }
+
+//     if(windowEnd >= pattern.length -1){
+//       // start to move
+//       const leftChar = input[windowStart]
+//       if(leftChar in freqMap){
+
+//         if(freqMap[leftChar] === 0 ){
+//           matchedCount -= 1
+//         }
+//         freqMap[leftChar] += 1
+//       }
+
+//       windowStart += 1
+//     }
+//   }
+
+
+//   return false
+// }
+
+
 export const permutation_in_a_string_qk = (input: string, pattern: string) => {
   let windowStart = 0, maxLength = 0;
   let freqMap: Record<string, number> = {}
-  let matchedCount = 0;
+  let matchedCount = 0
 
-  for(let index = 0; index < pattern.length; index += 1){
-    const char = pattern[index] //?
-    if(char in freqMap){
+
+  for (let index = 0; index < pattern.length; index += 1) {
+    const char = pattern[index]
+    if (char in freqMap) {
       freqMap[char] += 1
-    }else{
+    } else {
       freqMap[char] = 1
     }
   }
 
   freqMap
 
-  for(let windowEnd = 0; windowEnd < input.length; windowEnd += 1){
+  for (let windowEnd = 0; windowEnd < input.length; windowEnd += 1) {
     const rightChar = input[windowEnd]
-    
-    if(rightChar in freqMap){
+
+    if (rightChar in freqMap) {
 
       freqMap[rightChar] -= 1
-      if(freqMap[rightChar] === 0){
+      if (freqMap[rightChar] === 0) {
         matchedCount += 1
       }
 
-      if(matchedCount === Object.keys(freqMap).length){
-        return true // matched all letters
+      if (matchedCount === Object.keys(freqMap).length) {
+        return true
       }
     }
 
-    if(windowEnd >= pattern.length -1){
-      // start to move
-      const leftChar = input[windowStart]
-      if(leftChar in freqMap){
 
-        if(freqMap[leftChar] === 0 ){
+    if (windowEnd >= pattern.length - 1) {
+      // start moving 
+      const leftChar = input[windowStart]
+      if (leftChar in freqMap) {
+
+        if (freqMap[leftChar] === 0) {
           matchedCount -= 1
         }
+
         freqMap[leftChar] += 1
       }
 
@@ -92,11 +146,9 @@ export const permutation_in_a_string_qk = (input: string, pattern: string) => {
     }
   }
 
-
   return false
 }
 
-
-// console.log(permutation_in_a_string_qk('bcdxabcdy', 'bcdyabcdx'));
-// console.log(permutation_in_a_string_qk('oidbcaf', 'abc'));
-// console.log(permutation_in_a_string_qk('odicf', 'dc'));
+// console.log(permutation_in_a_string_qk('bcdxabcdy', 'bcdyabcdx')); // true
+// console.log(permutation_in_a_string_qk('oidbcaf', 'abc')); //true
+console.log(permutation_in_a_string_qk('odicf', 'dc')); //true

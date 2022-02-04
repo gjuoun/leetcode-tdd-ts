e480c51038c35a4ecdb63f47369a875aeb8b4d22
Jun Guo <gjuoun@gmail.com>
2022-02-03 22:44:43 -0400

    redo q2

--- src/slidingWindow/challenges/Challenge 2/question.quokka.ts
+++ src/slidingWindow/challenges/Challenge 2/question.quokka.ts
@@ -1,7 +1,59 @@
+// export const string_anagrams = (input: string, pattern: string) => {
+//   let windowStart = 0, maxLength = 0;
+//   let freqMap: Record<string, number> = {}
+//   let matchedCount = 0;
+
+//   for (let index = 0; index < pattern.length; index += 1) {
+//     const char = pattern[index]
+//     if (char in freqMap) {
+//       freqMap[char] += 1
+//     } else {
+//       freqMap[char] = 1
+//     }
+//   }
+
+//   freqMap
+//   const resultIndexes: number[] = []
+
+//   for (let windowEnd = 0; windowEnd < input.length; windowEnd += 1) {
+//     const rightChar = input[windowEnd]
+
+//     if (rightChar in freqMap) {
+
+//       freqMap[rightChar] -= 1
+
+//       if (freqMap[rightChar] === 0) {
+//         matchedCount += 1
+//       }
+
+//       if (matchedCount === Object.keys(freqMap).length) {
+//         resultIndexes.push(windowStart)
+//         resultIndexes
+//       }
+//     }
+
+//     if (windowEnd >= pattern.length - 1) {
+//       const leftChar = input[windowStart]
+
+//       if (leftChar in freqMap) {
+
+//         if (freqMap[leftChar] === 0) {
+//           matchedCount -= 1
+//         }
+//         freqMap[leftChar] += 1
+//       }
+
+//       windowStart += 1
+//     }
+//   }
+
+//   return resultIndexes
+
+// }
+
 export const string_anagrams = (input: string, pattern: string) => {
-  let windowStart = 0, maxLength = 0;
+  let windowStart = 0, matchedCount = 0;
   let freqMap: Record<string, number> = {}
-  let matchedCount = 0;
 
   for (let index = 0; index < pattern.length; index += 1) {
     const char = pattern[index]
@@ -12,9 +64,7 @@ export const string_anagrams = (input: string, pattern: string) => {
     }
   }
 
-  freqMap
-  const resultIndexes: number[] = []
-
+  const resultArr: number[] = []
   for (let windowEnd = 0; windowEnd < input.length; windowEnd += 1) {
     const rightChar = input[windowEnd]
 
@@ -27,28 +77,29 @@ export const string_anagrams = (input: string, pattern: string) => {
       }
 
       if (matchedCount === Object.keys(freqMap).length) {
-        resultIndexes.push(windowStart)
-        resultIndexes
+        resultArr.push(windowStart)
+        windowStart
       }
     }
+
 
     if (windowEnd >= pattern.length - 1) {
       const leftChar = input[windowStart]
 
-      if (leftChar in freqMap) {
+      if (rightChar in freqMap) {
 
         if (freqMap[leftChar] === 0) {
           matchedCount -= 1
         }
-        freqMap[leftChar] += 1
-      }
 
-      windowStart += 1
+        freqMap[leftChar] += 1
+
+        windowStart += 1
+      }
     }
   }
 
-  return resultIndexes
-
+  return resultArr //?
 }
 
 

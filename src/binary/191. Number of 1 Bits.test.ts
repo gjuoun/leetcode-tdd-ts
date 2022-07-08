606710fdd80b1703e59ef82489988785572719df
Jun Guo <gjuoun@gmail.com>
2022-07-07 22:43:55 -0300

    assert removed

--- src/binary/191. Number of 1 Bits.test.ts
+++ src/binary/191. Number of 1 Bits.test.ts
@@ -5,18 +5,18 @@ describe("191. Number of 1 Bits", () => {
     const input = parseInt("00000000000000000000000000001011", 2);
     const expected = 3;
     const rev = numberOfOneBits(input);
-    assert.equal(rev, expected);
+    expect(rev).toBe(expected);
   });
   it("Input: 00000000000000000000000010000000, Output: 1", () => {
     const input = parseInt("00000000000000000000000010000000", 2);
     const expected = 1;
     const rev = numberOfOneBits(input);
-    assert.equal(rev, expected);
+    expect(rev).toBe(expected);
   });
   it("Input: 11111111111111111111111111111101, Output: 31", () => {
     const input = parseInt("11111111111111111111111111111101", 2);
     const expected = 31;
     const rev = numberOfOneBits(input);
-    assert.equal(rev, expected);
+    expect(rev).toBe(expected);
   });
 });

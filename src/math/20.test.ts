d08d03ae4e31b193273c1bdac90f5fd479838265
Jun Guo <gjuoun@gmail.com>
2022-07-07 23:35:51 -0300

    update q20

---
+++ src/math/20.test.ts
@@ -0,0 +1,42 @@
+function isValid(s: string): boolean {
+  const stack: string[] = [];
+
+  const map: Record<string, string> = {
+    ")": "(",
+    "]": "[",
+    "}": "{",
+  };
+  for (let i = 0; i < s.length; i++) {
+    const curr = s[i];
+
+    if (curr in map) {
+      // matching closing bracket
+      const stackTopEl = stack[stack.length - 1];
+      const openingBracket = map[curr];
+      if (stackTopEl === openingBracket) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    } else {
+      stack.push(curr);
+    }
+  }
+  return stack.length === 0;
+}
+
+isValid("{[]}"); //?
+
+describe("20. Valid Parentheses", () => {
+  it("()", () => {
+    expect(isValid("()")).toBe(true);
+  });
+
+  it("()[]{}", () => {
+    expect(isValid("()")).toBe(true);
+  });
+
+  it("(]", () => {
+    expect(isValid("(]")).toBe(false);
+  });
+});
